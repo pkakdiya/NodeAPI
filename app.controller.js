1a40d404460d6ffb8fa8d2def70a483a432e8aa6
pkakadiy <priyank_kakadiya@bmc.com>
2019-04-17 14:27:13 +0530

    Add Middleware to check the JWT Token

--- app.controller.js
+++ app.controller.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const userService = require('./app.service');
+const middleware = require('./middleware');
 
 router.post('/authenticate', function(req, res, next){
     userService.handleAuthenticateRequest(req.body)
     .then(user => user ? res.json(user) : res.status(400).json({ message: 'Username or password is incorrect' }))
     .catch(err => next(err));
+});
+
+router.get('/main', middleware.checkToken, function(req, res, next) {
+    res.json({
+        success: true,
+        message: 'Main View'
+    })
 });
 
 module.exports = router;
